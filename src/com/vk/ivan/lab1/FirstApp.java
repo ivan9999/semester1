58fca1c7aa0a3a205a236d41ce50dceb527bcdac
ivan9999 <ximinez16@mail.ru>
2015-09-12 16:03:56 +0400

    Initial commit

---
+++ src/com/vk/ivan/lab1/FirstApp.java
@@ -0,0 +1,10 @@
+package com.vk.ivan.lab1;
+
+/**
+ * Created by User on 12.09.2015.
+ */
+public class FirstApp {
+    public static void main(String[] args) {
+        System.out.println(1+2+3+4+5);
+    }
+}
